daf2f5a2f75c6c8dbef13cfaed710a23664a2741
Nikhil <nikhilrewatkar58@gmail.com>
2026-09-12 15:17:03 +0530

    Fix Spring Boot context test

--- src/test/java/com/demo/InsuranceManagementSystemApplicationTests.java
+++ src/test/java/com/demo/InsuranceManagementSystemApplicationTests.java
@@ -3,7 +3,8 @@ package com.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.insurance.userservice.InsuranceManagementSystemApplication.class,
+                 properties = "spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration")
 class InsuranceManagementSystemApplicationTests {
 
 	@Test
